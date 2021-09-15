6beae16ec380b9a5e5f6859dd7adec97a3fd37e9
cbass2404 <cory.bass.07@gmail.com>
2021-09-15 13:55:10 -0500

    refactored test server setup

--- src/pages/entry/__tests__/Options.test.jsx
+++ src/pages/entry/__tests__/Options.test.jsx
@@ -1,15 +1,49 @@
 import { render, screen } from '@testing-library/react';
 
 import Options from '../Options';
+import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 
-beforeEach(() => render(<Options optionType='scoops' />));
+import { server } from '../../../mocks/servers';
+import { getScoops, getToppings } from '../../../mocks/handlers';
+
+beforeEach(() => server.listen());
+
+afterEach(() => server.close());
 
 describe('Options component renders and functions as follows', () => {
   it('displays an image for each scoop option from server', async () => {
+    server.use(getScoops());
+    render(
+      <OrderDetailsProvider>
+        <Options optionType='scoops' />
+      </OrderDetailsProvider>
+    );
+
     const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
     expect(scoopImages).toHaveLength(2);
 
     const altText = scoopImages.map((ele) => ele.alt);
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+  });
+
+  it('dispalys an image for each topping option from server', async () => {
+    server.use(getToppings());
+    render(
+      <OrderDetailsProvider>
+        <Options optionType='toppings' />
+      </OrderDetailsProvider>
+    );
+
+    const toppingImages = await screen.findAllByRole('img', {
+      name: /topping$/i,
+    });
+    expect(toppingImages).toHaveLength(3);
+
+    const altText = toppingImages.map((ele) => ele.alt);
+    expect(altText).toEqual([
+      'Cherries topping',
+      'M&Ms topping',
+      'Hot fudge topping',
+    ]);
   });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,12 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-// src/setupTests.js
-import { server } from './mocks/servers.js';
-
-beforeAll(() => server.listen());
-
-afterEach(() => server.resetHandlers());
-
-afterAll(() => server.close());
