9d2ac7ca87502677632c6ec6c6fba28544a161fc
cbass2404 <cory.bass.07@gmail.com>
2021-09-17 10:22:55 -0500

    built out test for toppings option

--- src/pages/entry/__tests__/totalUpdates.test.jsx
+++ src/pages/entry/__tests__/totalUpdates.test.jsx
@@ -26,3 +26,36 @@ it('updates scoop subtotal when scoops change', async () => {
   userEvent.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
+
+it('updates topping subtotal when toppings changes', async () => {
+  render(<Options optionType='toppings' />);
+
+  const toppingsSubtotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+  // check m&ms and check subtotal
+  const mmsInput = await screen.findByRole('checkbox', { name: 'M&Ms' });
+
+  // checkbox is unchecked by default
+  expect(mmsInput).not.toBeChecked();
+
+  // checking the box increases subtotal by $1.50
+  userEvent.click(mmsInput);
+  expect(mmsInput).toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+  // checking hot fudge increases the value to $3.00
+  const hotFudgeInput = await screen.findByRole('checkbox', {
+    name: 'Hot fudge',
+  });
+  expect(hotFudgeInput).not.toBeChecked();
+  userEvent.click(hotFudgeInput);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  // unchecking the box lowers the subtotal back to $1.50
+  userEvent.click(mmsInput);
+  expect(mmsInput).not.toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+});
